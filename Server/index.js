4d23cdf7b242082bcb4212cb9c661c35e5e786b9
King-AJr <talk2king.aj@gmail.com>
2023-08-15 09:28:44 +0100

    real-time communication

--- Server/index.js
+++ Server/index.js
@@ -10,13 +10,36 @@ const httpServer = createServer();
 
 const io = new Server(httpServer, {cors:{
     origin: 'http://localhost:3000',
+    AccessControlAllowOrigin: 'http://localhost:3000',
     allowedHeaders: ["Access-Control-Allow-Origin"],
     credentials: true
   }});
 
-io.on("connection", (socket) => {
-    console.log('A user connected');
-});
+  let connections = []
+  io.on('connect', (socket) => {
+      connections.push(socket);
+      console.log(`${socket.id} has connected`)
+      
+      socket.on('draw', (data) => {
+          connections.forEach(con => {
+              if (con.id !== socket.id) {
+                  con.emit('ondraw', data)
+              }
+          })
+      })
+      
+      socket.on('down', (data) => {
+          connections.forEach(con => {
+              if (con.id !== socket.id) {
+                  con.emit('ondown', {x: data.x, y: data.y})
+              }
+          })
+      })
+  })
+  
+  app.get('/', (req, res) => {
+    res.send.json('connected')
+  })
 
 httpServer.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
